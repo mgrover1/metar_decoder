1859980a6b4272bc0aaad6011bdbfdbeff43c450
Max Grover <maxi2312@gmail.com>
2019-06-06 21:03:41 -0600

    Initial files, currently working on tests

--- metar_parse.py
+++ metar_parse.py
@@ -9,25 +9,25 @@ def parse_metar(metar_text):
     Pandas Dataframe that can be subset easily
     """
 
-    #Import the neccessary libraries
+    # Import the neccessary libraries
     import pandas as pd
     import numpy as np
     from metar_decode import parse
     from metpy.units import units
 
-    #Build a dataframe that will store the data
+    # Build a dataframe that will store the data
     df = pd.DataFrame()
 
-    #Decode the data using the parser (built using Canopy)
+    # Decode the data using the parser (built using Canopy)
     tree = parse(metar_text)
 
-    #Set the station id
+    # Set the station id
     if tree.siteid.text == '':
         df['station_id'] = np.nan
     else:
         df['station_id'] = tree.siteid.text
 
-    #Set the datetime
+    # Set the datetime
     if tree.datetime.text == '':
         df['date_time'] = np.nan
         df['day'] = np.nan
@@ -38,7 +38,7 @@ def parse_metar(metar_text):
         df['day'] = int(datetime[0:3])
         df['time_utc'] = int(datetime[3:])
 
-    #Set the wind variables
+    # Set the wind variables
     if tree.wind.text == '':
         df['wind_dir'] = np.nan
         df['wind_spd'] = np.nan
@@ -47,8 +47,9 @@ def parse_metar(metar_text):
             df['wind_dir'] = np.nan
         else:
             df['wind_dir'] = int(tree.wind.wind_spd.text)
+            df['wind_spd'] = int(tree.wind.wind_dir.text)
 
-    #Set the weather symbols
+    # Set the weather symbols
     if tree.curwx.text == '':
         df['wx1'] = np.nan
         df['wx2'] = np.nan
@@ -58,7 +59,7 @@ def parse_metar(metar_text):
         df['wx1'] = wx[0]
         df['wx2'] = wx[1]
 
-    #Set the sky conditions
+    # Set the sky conditions
     if tree.skyc.text == '':
         df['skyc1'] = np.nan
         df['skyc2'] = np.nan
@@ -70,35 +71,29 @@ def parse_metar(metar_text):
         df['skyc2'] = skyc[1]
         df['skyc3'] = skyc[2]
 
-    #Set the temperature and dewpoint
+    # Set the temperature and dewpoint
     if tree.temp_dewp.text == '':
         df['temp'] = np.nan
         df['dewp'] = np.nan
     else:
         if "M" in tree.temp_dewp.temp.text:
-            df['temp'] = (-1*float(tree.temp_dewp.temp.text[-2:]))
+            df['temp'] = (-1 * float(tree.temp_dewp.temp.text[-2:]))
         else:
             df['temp'] = float(tree.temp_dewp.temp.text[-2:])
         if "M" in tree.temp_dewp.dewp.text:
-            df['dewp'] = (-1*float(tree.temp_dewp.dewp.text[-2:]))
+            df['dewp'] = (-1 * float(tree.temp_dewp.dewp.text[-2:]))
         else:
             df['dewp'] = float(tree.temp_dewp.dewp.text[-2:])
 
-    #Set the altimeter value
+    # Set the altimeter value
     if tree.altim.text == '':
         df['altim'] = np.nan
     else:
         if (float(tree.altim.text[2:6])) > 1100:
-            df['altim'] = (float(tree.altim.text[2:6])/100)*units("inHg").to('hPa').magnitude
+            df['altim'] = (float(tree.altim.text[2:6]) / 100) * units("inHg").to('hPa').magnitude
         else:
             df['altim'] = float(tree.altim.text[2:6])
 
 
-
-
-
-
-
-
-
-
+metar_text = 'K0CO 042354Z AUTO 01007KT M1/4SM -SN OVC001 M07/M13 A2992 RMK'
+parse_metar(metar_text)
